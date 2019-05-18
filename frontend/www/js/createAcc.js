af41cd9d559385405bcf40667dc646551a2a689b
prazd <alexokda@list.ru>
2019-05-18 02:40:15 -0400

    ...

--- frontend/www/js/createAcc.js
+++ frontend/www/js/createAcc.js
@@ -1,5 +1,7 @@
-function createAcc(){
+new ClipboardJS('.addr');
+new ClipboardJS('.prvt');
 
+function createAcc(){
     let pin = document.getElementById("pincode").value;
 
     let ls = new Storage.SecureLs({encodingType: 'aes', encryptionSecret:pin});
@@ -11,6 +13,10 @@ function createAcc(){
     let privateText =  document.getElementById("prvt");
     let addressText =  document.getElementById("addr");
 
-    privateText.innerText = `Your private key: ${randomWallet.privateKey}`;
-    addressText.innerText = `Your address : ${randomWallet.address}`
+    addressText.innerHTML = `<p style="font-size: 22px; word-wrap: break-word">Adress: \n<span id="addr">${randomWallet.address}</span><button data-clipboard-target="#addr" class="addr btn btn-success btn-sm">COPY</button></p><br>`;
+    privateText.innerHTML = `<p style="font-size: 22px; word-wrap: break-word">PrivateKey: \n<span id="prvt">${randomWallet.privateKey}</span><button data-clipboard-target="#prvt" class="prvt btn btn-success btn-sm">COPY</button></p><br>`;
+
+    let createButton = document.getElementById("createBtn");
+    createButton.style.display = "none";
+    // let pincode = document.getElementById("pincode");
 }
---
+++ frontend/www/js/utils.js
@@ -0,0 +1,21 @@
+async function req(method, url, data) {
+    const settings = {
+        async: true,
+        crossDomain: true,
+        url: url,
+        method: method,
+        processData: false
+    };
+
+    if (data) {
+        settings.data = data;
+        settings.headers = {
+            "Content-Type": "application/json"
+
+        };
+    }
+
+    return await $.ajax(settings);
+}
+
+
